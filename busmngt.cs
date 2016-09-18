9a7ef8aecc732aac75aefe89bd3a519d28450077
BusiManTeam1 <natalee32@yahoo.com>
2016-09-18 09:07:09 -0500

    Update busmngt.cs

--- busmngt.cs
+++ busmngt.cs
@@ -10,6 +10,18 @@ namespace busmngt
     {
         static void Main(string[] args)
         {
+        try
+        {
+        IEmployee Natalee = new EmployeeA("Natalee" 30, Security Analyst);
+        Console.Writeline("Information is incorrect");
+        Console.ReadKey();
+        }
+        catch
+        {
+            Console.Writeline("Employee Input Incorrect")
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
         }
     }
 }
